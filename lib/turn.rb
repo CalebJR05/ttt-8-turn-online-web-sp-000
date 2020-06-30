b594caf0ae0417811f286a30fa65ad4ceb378919
Caleb R <cjr4912@gmail.com>
2020-06-30 18:19:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,7 @@ end
 
 def valid_move(board, index)
   spot = index.to_i
-  if position_taken?(board, index) && spot.between?(0,8)
+  if !position_taken?(board, index) && spot.between?(0,8)
     true
     
   else
